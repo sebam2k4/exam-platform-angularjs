9ad1497c670632702ee938c8732744bda40c6924
sebam2k4 <sebam2k4@gmail.com>
2017-09-16 21:37:05 +0100

    Passing data between nav and exam page controllers

--- js/controller.js
+++ js/controller.js
@@ -1,29 +1,29 @@
 angular.module('RouteControllers', [])
 
-//parent controller
-// .controller('ParentController', function($scope) {
-//   $scope.name = 'Sebastian';
-// })
+  //parent controller
+  // .controller('ParentController', function($scope) {
+  //   $scope.name = 'Sebastian';
+  // })
 
-// Navigation Controller
-.controller('NavController' , function($scope,) {
+  // Navigation Controller
+  .controller('NavController', function ($scope, Data) {
+    $scope.Data = Data;
+  })
 
-})
-
-// ngView controllers
-.controller('HomeController', function($scope,) {
+  // ngView controllers
+  .controller('HomeController', function ($scope) {
     $scope.pageClass = 'page-home';
     $scope.heading = 'Validating Your Skills';
     $scope.subheading = 'Expert Examination Platform';
-})
-.controller('Page1Controller', function($scope) {
+  })
+  .controller('Page1Controller', function ($scope) {
     $scope.pageClass = 'page-page1';
     $scope.heading = 'Page 1';
     $scope.subheading = 'Subheading of Page 1';
-})
-.controller('Page2Controller', function($scope) {
+  })
+  .controller('Page2Controller', function ($scope, Data) {
     $scope.pageClass = 'page-exams';
     $scope.heading = 'Exams';
     $scope.subheading = 'Test Your Super Powers';
-
-})
+    $scope.Data = Data;
+  })
