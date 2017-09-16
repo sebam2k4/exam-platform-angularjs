4d4cd675892a93947fe983a6a69c52d1c926a13c
sebam2k4 <sebam2k4@gmail.com>
2017-09-16 21:37:23 +0100

    fixed sytax formating

--- js/app.js
+++ js/app.js
@@ -1,35 +1,35 @@
 angular.module('examApp', ['ngRoute', 'RouteControllers', 'ngAnimate', 'ui.materialize'])
-.config(function($locationProvider, $routeProvider) {
+  .config(function ($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true); // Enable href routing without hashes
 
     $routeProvider
-    .when('/', {
-    templateUrl: "templates/home.html",
-    controller: "HomeController"
-    })
-    .when('/page1', {
+      .when('/', {
+        templateUrl: "templates/home.html",
+        controller: "HomeController"
+      })
+      .when('/page1', {
         templateUrl: "templates/page1.html",
         controller: "Page1Controller"
-    })
-    .when('/exams', {
+      })
+      .when('/exams', {
         templateUrl: "templates/page2.html",
         controller: "Page2Controller"
-    })
-    .otherwise({ redirectTo: '/' });
-})
+      })
+      .otherwise({ redirectTo: '/' });
+  })
 
 
-// testing disappearing navigation on button click
-.factory('Data', function(){
+  // testing disappearing navigation on button click
+  .factory('Data', function () {
     return { firstName: '' };
-})
+  })
 
-// ngView autoscroll delay(due to css transition property) fix:
-// set timeout for scroll-to-top to 0 seconds to remove delay
-.run(function ($rootScope, $timeout, $window) {
+  // ngView autoscroll delay(due to css transition property) fix:
+  // set timeout for scroll-to-top to 0 seconds to remove delay
+  .run(function ($rootScope, $timeout, $window) {
     $rootScope.$on('$routeChangeSuccess', function () {
       $timeout(function () {
-        $window.scrollTo(0,0);
+        $window.scrollTo(0, 0);
       }, 0);
     });
-})
+  })
