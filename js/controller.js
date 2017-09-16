ba2a57e2f0f15b81a52e24c919ed9914972c129a
sebam2k4 <sebam2k4@gmail.com>
2017-09-16 21:21:05 +0100

    Defined pages; variables

--- js/controller.js
+++ js/controller.js
@@ -7,15 +7,23 @@ angular.module('RouteControllers', [])
 
 // Navigation Controller
 .controller('NavController' , function($scope,) {
+
 })
 
 // ngView controllers
 .controller('HomeController', function($scope,) {
-
+    $scope.pageClass = 'page-home';
+    $scope.heading = 'Validating Your Skills';
+    $scope.subheading = 'Expert Examination Platform';
 })
 .controller('Page1Controller', function($scope) {
-
+    $scope.pageClass = 'page-page1';
+    $scope.heading = 'Page 1';
+    $scope.subheading = 'Subheading of Page 1';
 })
 .controller('Page2Controller', function($scope) {
+    $scope.pageClass = 'page-page2';
+    $scope.heading = 'Page 2';
+    $scope.subheading = 'Subheading of Page 2';
 
 })
