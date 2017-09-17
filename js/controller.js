0c5dcb28157953c8992ee1bc43f1c078a9a50978
sebam2k4 <sebam2k4@gmail.com>
2017-09-17 03:53:45 +0100

    Loading JSON into $scope

--- js/controller.js
+++ js/controller.js
@@ -15,15 +15,29 @@ angular.module('RouteControllers', [])
     $scope.pageClass = 'page-home';
     $scope.heading = 'Validating Your Skills';
     $scope.subheading = 'Expert Examination Platform';
+    $scope.images = [];
   })
   .controller('Page1Controller', function ($scope) {
     $scope.pageClass = 'page-page1';
     $scope.heading = 'Page 1';
     $scope.subheading = 'Subheading of Page 1';
   })
-  .controller('Page2Controller', function ($scope, HideNav) {
+  .controller('Page2Controller', function ($scope, $http, HideNav) {
     $scope.pageClass = 'page-exams';
     $scope.heading = 'Exams';
     $scope.subheading = 'Test Your Super Powers';
     $scope.hideNav = HideNav;
+
+    $http({
+      method: 'GET',
+      url: '/data/exam1.json'
+    }).then(function successCallback(response) {
+      $scope.exam = response.data;
+    },
+    function errorCallback(response) {
+      console.log("Couldn't load JSON file");
+    });
+
+
+
   })
