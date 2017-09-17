f45cf5df75ce915bf9dc8016be84cad00a503244
sebam2k4 <sebam2k4@gmail.com>
2017-09-17 04:56:21 +0100

    ??? path???

--- js/controller.js
+++ js/controller.js
@@ -41,7 +41,7 @@ angular.module('AppRouteControllers', [])
 
     $http({
       method: 'GET',
-      url: 'https://github.com/sebam2k4/stream1-project/blob/gh-pages/data/exam1.json'
+      url: 'data/exam1.json'
     }).then(function successCallback(response) {
       $scope.exam = response.data;
     },
