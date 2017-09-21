4a466c00d5682ea4740cc4a0b58cab1122106698
sebam2k4 <sebam2k4@gmail.com>
2017-09-21 06:53:33 +0100

    Added code required by Materialize input-date & countries

--- js/controller.js
+++ js/controller.js
@@ -52,14 +52,46 @@ angular.module('RouteControllers', [])
     $scope.pageClass = '';
     $scope.heading = 'Register a New Account';
 
+    $scope.countries = ['United Kingdom', 'Ireland', 'USA', 'Spain', 'Sweden', 'Poland', 'Norway']
     $scope.submitForm = function(isValid) {
       //$scope.submitted = true;
       if (isValid) {
         //alert('Test: this form is amazing!');
         $scope.noUser = true;
-      } else {
-
       }
+    }
+
+    // Required for MaterializeCSS input-date directive
+    var currentTime = new Date();
+    $scope.currentTime = currentTime;
+    $scope.month = ['Januar', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    $scope.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    $scope.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+    $scope.disable = [false];
+    $scope.today = 'Today';
+    $scope.clear = 'Clear';
+    $scope.close = 'Close';
+    var days = 2190; // show 6 years
+    $scope.minDate = (new Date($scope.currentTime.getTime() - ( 0 ))).toISOString(); // show only dates in future
+    $scope.maxDate = (new Date($scope.currentTime.getTime() + ( 1000 * 60 * 60 *24 * days ))).toISOString();
+    $scope.onStart = function () {
+        console.log('onStart');
+    };
+    $scope.onRender = function () {
+        console.log('onRender');
+    };
+    $scope.onOpen = function () {
+        console.log('onOpen');
+    };
+    $scope.onClose = function () {
+        console.log('onClose');
+    };
+    $scope.onSet = function () {
+        console.log('onSet');
+    };
+    $scope.onStop = function () {
+        console.log('onStop');
     };
   })
 
