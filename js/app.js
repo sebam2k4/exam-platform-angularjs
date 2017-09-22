f8dc7cae125515773862319348d6cf9ad76a46f9
sebam2k4 <sebam2k4@gmail.com>
2017-09-22 21:39:50 +0100

    Added 'added date' to exams

--- js/app.js
+++ js/app.js
@@ -44,6 +44,7 @@ angular.module('examApp', ['ngRoute', 'AppRouteControllers', 'AppDirectives', 'n
 
   // SERVICES:
   // testing disappearing navigation on button click & toggle
+  // needed to pass either 'true' or 'false' value from ng-view controllers to trigger ng-hide in HideController.
   .factory('HideNav', function () {
     return { examInProgress: '' };
   })
@@ -57,19 +58,20 @@ angular.module('examApp', ['ngRoute', 'AppRouteControllers', 'AppDirectives', 'n
           provider: 'Code Institute',
           path: 'exams/exam-info',
           image: 'img/300x200.png',
+          addedDate: '2017-09-22',
           type: 'coding' 
         },
-        { examId: '2', name: 'Calculus', provider: 'The Mathmagicians', path: 'exams/exam-info', image: 'img/300x200.png', type: 'math' },
-        { examId: '3', name: 'Python', provider: 'The Monty Snake?', path: 'exams/exam-info', image: 'img/300x200.png', type: 'coding' },
-        { examId: '4', name: 'JavaScript Basics', provider: 'Code Institute', path: 'exams/exam-info', image: 'img/300x200.png', type: 'coding' },
-        { examId: '5', name: 'Routing & Switching', provider: 'Cisco', path: 'exams/exam-info', image: 'img/300x200.png', type: 'it_tech' },
-        { examId: '6', name: 'Office365', provider: 'Cool guys at Microsoft', path: 'exams/exam-info', image: 'img/300x200.png', type: 'productivity' },
-        { examId: '7', name: 'Secutity', provider: 'CompTIA', path: 'exams/exam-info', image: 'img/300x200.png', type: 'it_tech' },
-        { examId: '8', name: 'Debugging', provider: 'Rubber Duckie', path: 'exams/exam-info', image: 'img/300x200.png', type: 'coding' },
-        { examId: '9', name: 'Proper Etiquette', provider: 'Your Aunt Mary', path: 'exams/exam-info', image: 'img/300x200.png', type: 'other' },
-        { examId: '10', name: 'Machine Code', provider: 'CPU', path: 'exams/exam-info', image: 'img/300x200.png', type: 'coding' },
-        { examId: '11', name: 'UE4', provider: 'Unreal', path: 'exams/exam-info', image: 'img/300x200.png', type: 'other' },
-        { examId: '12', name: 'Six Sigma', provider: 'Black Belt Coach', path: 'exams/exam-info', image: 'img/300x200.png', type: 'productivity' }
+        { examId: '2', name: 'Calculus', provider: 'The Mathmagicians', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-03-01', type: 'math' },
+        { examId: '3', name: 'Python', provider: 'The Monty Snake?', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-04-23', type: 'coding' },
+        { examId: '4', name: 'JavaScript Basics', provider: 'Code Institute', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-08-12', type: 'coding' },
+        { examId: '5', name: 'Routing & Switching', provider: 'Cisco', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-03-27', type: 'it_tech' },
+        { examId: '6', name: 'Office365', provider: 'Cool guys at Microsoft', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-07-06', type: 'productivity' },
+        { examId: '7', name: 'Secutity', provider: 'CompTIA', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-09-21', type: 'it_tech' },
+        { examId: '8', name: 'Debugging', provider: 'Rubber Duckie', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-09-16', type: 'coding' },
+        { examId: '9', name: 'Proper Etiquette', provider: 'Your Aunt Mary', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-09-02', type: 'other' },
+        { examId: '10', name: 'Machine Code', provider: 'CPU', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-08-11', type: 'coding' },
+        { examId: '11', name: 'UE4', provider: 'Unreal', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-05-23', type: 'other' },
+        { examId: '12', name: 'Six Sigma', provider: 'Black Belt Coach', path: 'exams/exam-info', image: 'img/300x200.png', addedDate: '2017-02-28', type: 'productivity' }
       ];
   })
 
